2d0389887251f6693f376ce867fc3d1ad8558824
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-01-06 08:11:45 +0000

    bitcoin ticker

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { useQuery } from "react-query";
 
-function App() {
+import { Wrapper } from "./styles/App.styles";
+
+type BitcoinData = {
+  "15m": number;
+  buy:number;
+  last:number;
+  sell:number;
+  symbol:string;
+}
+
+type Currencies = {
+  [key: string] : BitcoinData
+}
+
+const getBCData = async ():Promise <Currencies> => 
+await(await fetch('https://blockchain.info/ticker')).json();
+
+
+
+const INTERVAL_TIME = 30000; // 30 SECONDS
+
+const App =() => {
+  const [currency, setCurrency] = useState('USD');
+  const {data, isLoading, error, refetch} = useQuery<Currencies>('bc-data', getBCData)
+
+
+  const handleCurrencySelection = (e:any) => {
+    setCurrency(e.target.value)
+  }
+
+  useEffect(() => {
+    const interval = setInterval(refetch, INTERVAL_TIME);
+    return () => clearInterval(interval)
+  }, [refetch])
+
+
+
+  if(isLoading) return<div>...Is Loading</div>
+  if(error) return<div>something went wronf</div>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Wrapper>
+    <>
+    <h2>Bitcoin Price</h2>
+    <select value={currency} onChange={handleCurrencySelection}>
+{data && Object.keys(data).map(currency => (
+  <option key={currency} value={currency}>
+    {currency}
+  </option>
+))}
+
+    </select>
+    <div>
+      <h2>
+        {data && data[currency].symbol}
+        {data && data[currency].last}
+      </h2>
     </div>
+    </>
+    </Wrapper>
+
+
   );
 }
 
